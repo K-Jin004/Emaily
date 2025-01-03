c84e0c9ee48c19f3e2d7c2c3a82544ceb64246c0
Xiangyu Jin <xjin13@illinois.edu>
2025-01-03 14:51:36 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+// import express from 'express' new version
+
+
+// app: express app to register this route handler with
+// get: watch for incoming requests withi this method
+// '/': watch for request trying to access '/'
+// req: object representing the incoming request
+// res: object representing the outgoing response
+// res.send({}): immediately send some JSON back to who ever made this request
+//接受localhost:8432/ 请求时，发送hi there
+app.get('/', (req, res) => {
+    res.send({hi: 'there'});
+});
+
+const PORT = process.env.PORT || 8432;
+app.listen(PORT); // express tells Node to listen on port 8432
+
